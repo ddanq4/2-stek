a82bf1e3ec84e0ab8ee668a68fb80411141d0bf6
danq <danylo.kiriienko@student.karazin.ua>
2024-11-01 23:51:47 +0100

    ffff

--- my_fist_gem/charscounter/spec/test.rb
+++ my_fist_gem/charscounter/spec/test.rb
@@ -1,12 +1,10 @@
-
 require "charscounter"
 require "rspec"
 
-
 RSpec.describe Charscounter do
-  it "проверяет подсчет символов в строке hello" do
-    result = Charscounter.charscounter("hello")
+  it "рахує символи" do
+    result = Charscounter.charscounter("asjhdgajdyryryryrnsdjcvbvhsdgtfv][ad")
     puts result
-    expect(result).to eq({"h" => 1, "e" => 1, "l" => 2, "o" => 1})
+    expect(result).to eq({ "a" => 4, "s" => 4, "j" => 4, "h" => 3, "d" => 5, "g" => 3, "y" => 6, "r" => 5, "n" => 1, "c" => 1, "v" => 3, "b" => 2, "[" => 1, "]" => 1, "t" => 1, "f" => 1 })
   end
 end
